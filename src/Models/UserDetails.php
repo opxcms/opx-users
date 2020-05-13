42c30eab39ca93b6a09b998161c9c8f11f77422e
lozovoyv <lozovoyv@gmail.com>
2020-05-13 14:01:56 +0300

    Update models

--- src/Models/UserDetails.php
+++ src/Models/UserDetails.php
@@ -2,13 +2,7 @@
 
 namespace Modules\Opx\Users\Models;
 
-use Core\Foundation\Auth\Contracts\UserContract;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Foundation\Auth\Access\Authorizable;
-use Illuminate\Notifications\Notifiable;
-use Modules\Opx\Users\Traits\Authenticatable;
-use Illuminate\Contracts\Auth\Authenticatable as AuthContract;
 
 class UserDetails extends Model
 {
