c19953b902bc0187a871b92890344163a366fa7e
lozovoyv <lozovoyv@gmail.com>
2020-05-13 14:00:02 +0300

    Add and update locales

---
+++ src/Lang/ru/forms.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'login_title' => 'Вход',
+    'login_action_title' => 'Войти',
+    'register_title' => 'Регистрация',
+    'register_action_title' => 'Зарегистрироваться',
+    'forgot_title' => 'Восстановление пароля',
+    'forgot_action_title' => 'восстановить',
+    'reset_title' => 'Изменение пароля',
+    'reset_action_title' => 'изменить',
+
+    'email_title' => 'Email',
+    'password_title' => 'Пароль',
+    'password_confirm_title' => 'Подтверждение пароля',
+    'last_name_title' => 'Фамилия',
+    'first_name_title' => 'Имя',
+    'phone_title' => 'Мобильный телефон',
+    'remember_title' => 'Запомнить меня',
+
+    'error_phone_format' => 'Необходимо заполнить правильно.',
+    'error_required' => 'Необходимо заполнить.',
+    'error_password_length' => 'Пароль должен быть длиной не менее :length символов.',
+    'error_password_match' => 'Пароль должен совпадать с подтверждением.',
+];
